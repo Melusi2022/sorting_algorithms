c9147e783d75b89776dfe8af48793d54f42512d8
Melusi2022 <melusimedia@gmail.com>
2023-12-19 02:38:00 -0800

    Bubble sort

---
+++ sorting_validation.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ * sorting_validation - checks if list is sorted
+ * algorithm
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void sorting_validation(int *array, size_t size)
+{
+
+        if (!array || size < 2)
+        return;
+
+}
